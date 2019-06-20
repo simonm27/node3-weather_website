5ad932ed5066d0a414954615be6c1d0dfe03d6cc
Simon Minifie <simon@minifie.me>
2019-06-20 14:11:25 +0100

    returned data from forecast changed to add additional detail

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,7 +14,9 @@ const url = 'https://api.darksky.net/forecast/f3ba037745e2aaaad67b97e0d22014e4/'
                 callback('Unable to find location', undefined)
             }
             else {
-                callback(undefined, body.daily.data[0].summary + ' Current temperature is ' + body.currently.temperature + ' centigrade. There is a ' + body.currently.precipProbability + '% chance of rain.')
+                callback(undefined, body.daily.data[0].summary + ' Current temperature is ' + body.currently.temperature + 
+                ' centigrade. The highest temperature is expected to be ' + body.daily.data[0].temperatureHigh + ' centigrade, and the lowest is '
+                + body.daily.data[0].temperatureLow + ' centigrade. There is a ' + body.currently.precipProbability + '% chance of rain.')
             }
     })
 }
